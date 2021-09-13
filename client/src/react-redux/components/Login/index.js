4b2c8792127a76f3ea19d3a8392bb661d717d8b9
Todd Trowbridge <84187785+todd-trowbridge@users.noreply.github.com>
2021-09-13 16:58:40 -0400

    working login

--- client/src/react-redux/components/Login/index.js
+++ client/src/react-redux/components/Login/index.js
@@ -1,11 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Alert, Button, Card, Container, Form } from 'react-bootstrap'
+import { useHistory } from 'react-router'
+import { Link } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { actionLoggedIn } from '../User/actions'
 
-function Login() {
+export default function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    fetch('/api/v1/users/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email,
+        password
+      })
+    })
+      .then(res => res.json())
+      .then(data => {
+        if (data.error) {
+          setError(data.error)
+        } else {
+          dispatch(actionLoggedIn(data.user))
+          history.push('/')
+        }
+      })
+  }
+
   return (
-    <div>
-      Login
-    </div>
+    <Container>
+      { error && (<Alert variant="danger">{error}</Alert>) }
+      <Card>
+        <Card.Body>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3">
+              <Form.Label>Email</Form.Label>
+              <Form.Control value={email} onChange={e => setEmail(e.target.value)} type="text" placeholder="email@domain.com" />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Password</Form.Label>
+              <Form.Control value={password} onChange={e => setPassword(e.target.value)} type="password" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Login
+            </Button>
+          </Form>
+        </Card.Body>
+        <Card.Footer>
+          Don't have an account? <Link to="/register">Register Here</Link>
+        </Card.Footer>
+      </Card>
+    </Container>
   )
 }
-
-export default Login
