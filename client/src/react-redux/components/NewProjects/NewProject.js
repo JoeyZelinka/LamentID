f8127efb8ac30f27e4c4778c725f3f04bd7f1776
Joey Zelinka <cheflife365@gmail.com>
2021-09-15 15:20:02 -0400

    new project form, it's fine I guess

--- client/src/react-redux/components/NewProjects/NewProject.js
+++ client/src/react-redux/components/NewProjects/NewProject.js
@@ -1,17 +1,19 @@
 import React, { useState } from 'react'
 import { useHistory } from 'react-router';
+import { Button, Container, Form, Col } from 'react-bootstrap'
+import "./index.css"
 
 
 export default function NewProject() {
-    const [ projectName, setProjectName ] = useState("");
-    const [ error, setError ] = useState("");
-    const [ keywords, setKeywords ] = useState([{
+    const [projectName, setProjectName] = useState("");
+    const [error, setError] = useState("");
+    const [keywords, setKeywords] = useState([{
         searchTerm: "",
         subreddit: ""
     }]);
     const history = useHistory();
 
-    
+
     const handleSubmit = (e) => {
         e.preventDefault();
         fetch("/api/v1/projects/new", {
@@ -24,22 +26,22 @@ export default function NewProject() {
                 keywords: keywords
             })
         })
-        .then((res) => res.json())
-        .then((data) => {
-            if (data.error) {
-                setError(data.error)
-            } else { 
-                history.push("/dashboard")
-            }
-        })
+            .then((res) => res.json())
+            .then((data) => {
+                if (data.error) {
+                    setError(data.error)
+                } else {
+                    history.push("/dashboard")
+                }
+            })
     }
-    
+
     const updateKeyword = (index, key, value) => {
         const newKeywords = [...keywords];
         newKeywords[index][key] = value;
         setKeywords(newKeywords)
     }
-    
+
     //? add button to add rows and add new object to state
     const addNewKeywords = () => {
         setKeywords([...keywords, {
@@ -49,46 +51,49 @@ export default function NewProject() {
     }
 
     return (
-        <div className="newProjectForm">
-            { error && (<div>{error}</div>) }
-            <form 
-                onSubmit={handleSubmit}
-                className="newProject">
-                <p>Project Name</p>
-                <input 
-                    value={projectName} 
-                    name="keywords" 
-                    onChange={(e) => setProjectName(e.target.value)}
-                    type="text" 
-                    maxLength="100" />
-                <br/>
-                {keywords.map((keyword, index) => {
-                    return ( 
-                        <div key={index}>
-                            <label for="keywords">Keyword:</label>
-                            <input 
-                                value={keyword.searchTerm} 
-                                onChange={(e) => updateKeyword(index, "searchTerm", e.target.value)}
-                                name="keywords" 
-                                type="text" 
-                                maxLength="100" />
-                            <label for="subreddit">Subreddit:</label>
-                            <input 
-                                value={keyword.subreddit} 
-                                onChange={(e) => updateKeyword(index, "subreddit", e.target.value)}
-                                name="subreddit" 
-                                type="text" 
-                                maxLength="100" />
-                        </div>
+        <Container fluid className="newProjectForm">
+            {error && (<div>{error}</div>)}
+            <Col>
+                <Form>
+                    <Form.Group
+                        onSubmit={handleSubmit}
+                        className="newProject">
+                        <Form.Label>Project Name:</Form.Label>
+                        <Form.Control
+                            value={projectName}
+                            name="keywords"
+                            onChange={(e) => setProjectName(e.target.value)}
+                            type="text"
+                            maxLength="100" />
+
+                        {keywords.map((keyword, index) => {
+                            return (
+                                <Container key={index}>
+
+                                    <Form.Label for="keywords">Keyword:</Form.Label>
+                                    <Form.Control
+                                        value={keyword.searchTerm}
+                                        onChange={(e) => updateKeyword(index, "searchTerm", e.target.value)}
+                                        name="keywords"
+                                        type="text"
+                                        maxLength="100" />
+                                    <Form.Label for="subreddit">Subreddit:</Form.Label>
+                                    <Form.Control
+                                        value={keyword.subreddit}
+                                        onChange={(e) => updateKeyword(index, "subreddit", e.target.value)}
+                                        name="subreddit"
+                                        type="text"
+                                        maxLength="100" />
+                            </Container>
                     )
                 })}
-                <button onClick={addNewKeywords}>+</button>
-                {/* <br/>
-                <p>Suggested Keywords/SubReddits</p>
-                <textarea  name="suggest" cols="20" rows="4"></textarea> */}
-                <br/>
-                <button type="submit">Save Project</button>
-            </form>
-        </div>
+                    <Container className="buttons">
+                        <Button variant="warning" onClick={addNewKeywords}>+</Button>
+                        <Button variant="primary" type="submit">Save Project</Button>
+                    </Container>
+                </Form.Group>
+            </Form>
+              </Col>  
+        </Container>
     )
 }
