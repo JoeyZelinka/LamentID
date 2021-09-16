8d5e539878c04a8325408a2aa6d68cc112428853
Joey Zelinka <cheflife365@gmail.com>
2021-09-16 12:12:04 -0400

    login/registration modals in navbar

--- client/src/react-redux/components/NavBar/index.js
+++ client/src/react-redux/components/NavBar/index.js
@@ -1,17 +1,31 @@
-import React from 'react'
-import { BrowserRouter as Router, Link, Route, Switch  } from 'react-router-dom'
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
 import Register from '../Register/index'
 import Login from '../Login/index'
 import About from '../About/index'
 import NewProjects from '../NewProjects/NewProject'
 import Dashboard from '../Dashboard'
-import { Container, Nav, Navbar } from 'react-bootstrap'
+import { Container, Nav, Navbar, Button, Modal } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import { actionLoggedOut } from '../User/actions'
+
 
 function NavBar() {
   const { checked, user } = useSelector(state => state.user);
   const dispatch = useDispatch();
+
+  //modal variables start
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const [show2, setShow2] = useState(false);
+  const handleClose2 = () => setShow2(false);
+  const handleShow2 = () => setShow2(true);
+  //modal variables end
+ 
+
+
 
   const handleLogout = (e) => {
     e.preventDefault();
@@ -33,16 +47,54 @@ function NavBar() {
               <Nav className="ms-auto">
                 <Nav.Link as={Link} to="/">Home</Nav.Link>
                 <Nav.Link as={Link} to="/about">About</Nav.Link>
-                { checked && user ? (
+                {checked && user ? (
                   <>
-                  <Nav.Link as={Link} to="/newprojects">Start New Project</Nav.Link>
-                  <Nav.Link as={Link} to="/dashboard">Dashboard</Nav.Link>
-                  <Nav.Link href="/logout" onClick={handleLogout}>Logout</Nav.Link>
+                    <Nav.Link as={Link} to="/newprojects">Start New Project</Nav.Link>
+                    <Nav.Link as={Link} to="/dashboard">Dashboard</Nav.Link>
+                    <Nav.Link href="/logout" onClick={handleLogout}>Logout</Nav.Link>
                   </>
                 ) : (
-                <Nav.Link as={Link} to="/login">Login</Nav.Link>
-                ) }
-                </Nav>
+                  // <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                  <Container>
+                  
+                    <Button variant="link" onClick={handleShow}>
+                      Login
+                    </Button>
+
+                    <Modal show={show} onHide={handleClose}>
+                      
+                        <Modal.Title>Enter Login Info</Modal.Title>
+                     
+                      <Modal.Body><Login/></Modal.Body>
+                      <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                          Close
+                        </Button>
+                        
+                      </Modal.Footer>
+                    </Modal>               
+                                  
+                  <Button variant="link" onClick={handleShow2}>
+                    Register
+                    </Button>
+                  <Modal show={show2} onHide={handleClose2}>
+                    
+                      <Modal.Title>Enter Registration Info</Modal.Title>
+                   
+                    <Modal.Body><Register/></Modal.Body>
+                    <Modal.Footer>
+                      <Button variant="secondary" onClick={handleClose2}>
+                        Close
+                      </Button>
+                      
+                    </Modal.Footer>
+                  </Modal>
+                  </Container>
+                  )}
+                  
+
+                
+              </Nav>
             </Navbar.Collapse>
           </Container>
         </Navbar>
@@ -65,7 +117,7 @@ function NavBar() {
             </Route>
           </Switch>
         </div>
-    </Router>
+      </Router>
     </nav>
   )
 }
