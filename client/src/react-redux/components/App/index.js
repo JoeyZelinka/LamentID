21ac2041805d0a4c58588d71695b79b7aacf6389
Joey Zelinka <cheflife365@gmail.com>
2021-09-20 15:39:02 -0400

    ok I'm done with this page

--- client/src/react-redux/components/App/index.js
+++ client/src/react-redux/components/App/index.js
@@ -1,12 +1,12 @@
 import "./index.css";
 import NavBar from '../NavBar/index'
 
-import LandingCarousel from '../LandingCarousel/index'
+
 
 
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Graph from "../Graph/index"
+
 
 function App() {
   return (
