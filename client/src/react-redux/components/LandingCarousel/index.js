7cff6134b5d03ddf933dfcf3d40a9f5b2f9434e7
Joey Zelinka <cheflife365@gmail.com>
2021-09-20 14:56:12 -0400

    final animation. homepage component and route

--- client/src/react-redux/components/LandingCarousel/index.js
+++ client/src/react-redux/components/LandingCarousel/index.js
@@ -3,37 +3,47 @@ import { Card, Container, Row, Col } from 'react-bootstrap'
 import "./index.css"
 import { motion } from 'framer-motion'
 
-export default function LandingCarousel() {
 
+export default function LandingCarousel() {
+    const variants = {
+        hidden: {
+            opacity: 0,
+            x: 100,
+            y: 100,
+        },
+        show: {
+            opacity: 1,
+            x: 0,
+            y: 0,
+            transition: {
+                duration: 5,
+                staggerChildren: 0.5
+            }
+        }
+    }
     return (
         <Container>
             <motion.div
-                initial={{
-                    opacity: 0,
-                    x: 100, 
-                    y: 100,
-                }}
-                animate={{
-                    opacity: 1,
-                    x: 0,
-                    y: 0,
-                }}
-                transition={{
-                    duration: 5
-                }}
+                variants={variants}
+                initial="hidden"
+                animate="show"
                 className="bigdivstyle">
                 <Card className="trackyour w-25">
                     <Card.Body>Track Your:</Card.Body>
                 </Card>
-           
-           
-                <Row>       
-                    <Col>
-                    <Card.Text>Brand</Card.Text><img src="/swoosh.png" alt=""></img></Col>
-                    <Col><Card.Text>Product</Card.Text><img src="/af1.jpg" alt=""></img></Col>
-                    <Col><Card.Text>Future Purchase</Card.Text><img src="/corvette.jpg" alt=""></img></Col>       
+
+
+                <Row className="bpf">
+                    <Col><Card.Text>Brand</Card.Text><motion.img variants={variants} src="/swoosh.png" alt=""></motion.img></Col>
+                    <Col><Card.Text>Product</Card.Text><motion.img variants={variants} src="/af1.jpg" alt=""></motion.img></Col>
+                    <Col><Card.Text>Future Purchase</Card.Text><motion.img variants={variants} src="/corvette.jpg" alt=""></motion.img></Col>
                 </Row>
-                </motion.div>
+                <Row className="software">
+                    <Card.Text>With our sentiment tracking software:</Card.Text>
+                    <motion.img variants={variants} src="/dummygraph.png" alt=""></motion.img>
+                </Row>
+
+            </motion.div>
         </Container>
     )
 
